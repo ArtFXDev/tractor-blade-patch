549ceb55be400a812b86bbd743a8e0a6565161ec
Joseph HENRY <josephenry@protonmail.com>
2022-05-28 19:10:41 +0200

    docs: add comments

--- open_blade_port.ps1
+++ open_blade_port.ps1
@@ -1,3 +1,8 @@
+# Set auto restart for service
 Set-Itemproperty -path 'HKLM:\SYSTEM\CurrentControlSet\Services\Pixar Tractor Blade Service 2.4' -Name 'Start' -value '2'
+
+# Run the service
 Set-Service -Name "Pixar Tractor Blade Service 2.4" -Status Running -PassThru
+
+# Open port 9005 on TCP
 New-NetFirewallRule -DisplayName 'TractorBlade' -Profile Any -Direction Inbound -Action Allow -Protocol TCP -LocalPort @('9005')
